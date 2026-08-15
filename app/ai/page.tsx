46a1348267263d2a69cd48405d18dbbf329ccce1
Geo <youremail@gmail.com>
2026-08-15 14:42:38 +0900

    Add Ai Media RnD project

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -4,6 +4,15 @@ import { ProjectGrid } from "@/components/project-grid"
 const aiProjects = [
   {
     id: 1,
+    title: "Ai_Media RnD",
+    thumbnail: "/thumbnails/Ai_Media.png",
+    videoUrl: "https://youtu.be/Scw8c84zlr0",
+    description: `
+Ai Mediaart RnD
+`,
+  },
+  {
+    id: 2,
     title: "Touch Designer Interactive AI",
     thumbnail: "/thumbnails/Interactive_TD.jpg",
     videoUrl: "https://youtu.be/LwHMVYKbKH4",
@@ -12,7 +21,7 @@ SD Turbo model
 `,
   },
   {
-    id: 2,
+    id: 3,
     title: "Unreal Interactive Particle",
     thumbnail: "/thumbnails/media_art.jpg",
     videoUrl: "https://www.youtube.com/watch?v=xWxMG0UgdBI",
@@ -21,7 +30,7 @@ My first Media art test
 `,
   },
   {
-    id: 3,
+    id: 4,
     title: "One image, Full motion",
     thumbnail: "/thumbnails/Gaussian_Houdini.png",
     videoUrl: "https://youtu.be/_5RyjAx2ra0",
@@ -30,7 +39,7 @@ One image, Full motion using Gaussian splatting with Houdini.
 `,
   },
   {
-    id: 4,
+    id: 5,
     title: "LG-Global POV Contents",
     thumbnail: "/thumbnails/LG_AI_V02.jpg",
     videoUrl: "https://www.youtube.com/shorts/hKf5k8ZIu_A",
@@ -39,7 +48,7 @@ LG-Global POV Series.
 `,
   },
   {
-    id: 5,
+    id: 6,
     title: "BloomXCircle Project Gucci Style",
     thumbnail: "/thumbnails/Gucci.png",
     description: `
@@ -53,7 +62,7 @@ Patterns & Motifs: Flora / Nature / Animal Motifs, Green–Red–Green Stripe, B
 `,
   },
   {
-    id: 6,
+    id: 7,
     title: "BloomXCircle Project Dior Style",
     thumbnail: "/thumbnails/Dior.png",
     description: `
@@ -67,7 +76,7 @@ Patterns & Motifs: Oblique Monogram, Toile de Jouy
 `,
   },
   {
-    id: 7,
+    id: 8,
     title: "BloomXCircle Project D&G Style",
     thumbnail: "/thumbnails/D&G.png",
     description: `
