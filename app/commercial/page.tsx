7b84e9e7fe4e2c96ccb27f8d033514cf0f43ccc8
Geo <youremail@gmail.com>
2026-08-15 14:19:15 +0900

    Add Lotte commercial project

--- app/commercial/page.tsx
+++ app/commercial/page.tsx
@@ -93,12 +93,19 @@ const commercialProjects = [
     videoUrl: "https://youtu.be/Dkzzr3xnTmM",
     description: "Fx_BehindCut",
   },
-    {
+  {
     id: 14,
     title: "LH",
     thumbnail: "/thumbnails/LH.png",
     videoUrl: "https://www.youtube.com/watch?v=91-q8O021Eg",
     description: "Paper house Rig&Anim",
+  },
+  {
+    id: 15,
+    title: "Lotte",
+    thumbnail: "/thumbnails/Lotte.png",
+    videoUrl: "https://youtu.be/2BuR590JI4o",
+    description: "Fx",
   },
 ]
 
